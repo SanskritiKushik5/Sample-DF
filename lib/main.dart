10687f585a052c0e6df24c9e967aeb0007ad46e1
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-01 22:57:42 +0530

    Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,24 @@ void main() =>
       appBar: AppBar(
         title: const Text('First App'),
         centerTitle: true,
+        backgroundColor: Colors.deepOrange[900],
       ),
       body: const Center(
-        child: Text('Hellozzzzzzzzzzzz'),
+        child: Text(
+          'Hello',
+          style: TextStyle(
+            fontSize: 60.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: 'Indieflower',
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: const Text('click'),
+        backgroundColor: Colors.deepOrange[900],
       ),
     ),
   ));
