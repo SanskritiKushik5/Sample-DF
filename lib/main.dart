4d097bee649f6ad97470b971bf421e33c021c38a
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-02 10:31:53 +0530

    Images & assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() =>
-  runApp(MaterialApp(
-    home: Home()
-  ));
+    runApp(MaterialApp(
+        home: Home()
+    ));
 
 class Home extends StatelessWidget {
   // const Home({Key? key}) : super(key: key);
@@ -16,24 +16,19 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.deepOrange[900],
       ),
-      body: const Center(
-        child: Text(
-          'Hello',
-          style: TextStyle(
-            fontSize: 60.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey,
-            fontFamily: 'Indieflower',
-          ),
+      body: Center(
+        // child: Image(
+        //   // image: NetworkImage('https://images.unsplash.com/photo-1537420327992-d6e192287183?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),
+        //   image: AssetImage('assets/space-1.jpg') 
+        // ),
+          child: Image.asset('assets/space-2.jpg'),
         ),
-      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
         child: const Text('click'),
         backgroundColor: Colors.deepOrange[900],
-      ),
+      )
     );
   }
 }
-
