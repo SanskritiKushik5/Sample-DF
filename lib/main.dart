c7308784e6f7f3720a60666922acde5059997431
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-11-05 10:31:44 +0530

    Extracting widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
-void main() =>
-    runApp(const MaterialApp(
-      home: QuoteList(),
-    ));
+void main() => runApp(MaterialApp(
+    home: QuoteList()
+));
 
 class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
   @override
   _QuoteListState createState() => _QuoteListState();
 }
@@ -16,51 +14,22 @@ class QuoteList extends StatefulWidget {
 class _QuoteListState extends State<QuoteList> {
 
   List<Quote> quotes = [
-    Quote(text: 'Be yourself; everyone else is already taken', author: 'Oscar Wilde'),
-    Quote(text: 'I have nothing to declare except my genius', author: 'Oscar Wilde'),
-    Quote(text: 'The truth is rarely pure and never simple', author: 'Oscar Wilde')
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
-
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('Awesome Quotes'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote)=> quoteTemplate(quote)).toList(),
+        children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
       ),
     );
   }
